22eae513c7f0356de51bea0748d048fc5f4661e7
adamccaffrey <adam_mccaffrey@hotmail.co.uk>
2020-02-24 16:59:00 +0000

    Update CharController.cs
    change CharController script to include sound

--- Assets/Scripts/CharController.cs
+++ Assets/Scripts/CharController.cs
@@ -4,14 +4,18 @@ using UnityEngine;
 
 public class CharController : MonoBehaviour
 {
+
+    public LibPdInstance pdPatch;
     [SerializeField]
     float moveSpeed = 4f;
     Vector3 forward, right;
 
+    float velocity;
+
     // Start is called before the first frame update
     void Start()
     {
-        forward = Camera.main.transform.forward;    
+        forward = Camera.main.transform.forward;
         forward.y = 0;
         forward = Vector3.Normalize(forward);
         right = Quaternion.Euler(new Vector3(0, 90, 0)) * forward;
@@ -20,11 +24,22 @@ public class CharController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if(Input.anyKey)
-        Move();
+        if (Input.anyKey)
+        {
+            Move();
+            pdPatch.SendFloat("velocity", Mathf.Clamp(velocity, 0, 1));
+        }
+        else
+        {
+             pdPatch.SendFloat("velocity", 0);
+        }
     }
     void Move()
     {
+        // Vector3 direction = new Vector3(Input.GetAxis("HorizontalKey"), 0, Input.GetAxis("VerticalKey"));
+        // Vector3 rightMovement = right * moveSpeed * Time.deltaTime * Input.GetAxis("HorizontalKey");
+        // Vector3 upMovement = forward * moveSpeed * Time.deltaTime * Input.GetAxis("VerticalKey");
+
         Vector3 direction = new Vector3(Input.GetAxis("Horizontal"), 0, Input.GetAxis("Vertical"));
         Vector3 rightMovement = right * moveSpeed * Time.deltaTime * Input.GetAxis("Horizontal");
         Vector3 upMovement = forward * moveSpeed * Time.deltaTime * Input.GetAxis("Vertical");
@@ -32,7 +47,11 @@ public class CharController : MonoBehaviour
 
         transform.forward = heading;
         transform.position += rightMovement;
-        transform.position += upMovement; 
+        transform.position += upMovement;
+        velocity = moveSpeed * Mathf.Clamp(Mathf.Max(Mathf.Abs(heading.x), Mathf.Abs(heading.z)), 0f, 0.7f);
+
+        Debug.Log("velocity" + velocity);
+        // pdPatch.SendFloat("velocity", Mathf.Clamp(velocity, 0, 1));
     }
 }
 
